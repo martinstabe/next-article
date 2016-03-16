c0f84478952973c08e95e5ba67862c072b58ef25
andygnewman <andrew.newman@ft.com>
2016-03-16 10:56:14 +0000

    fix errors when missing tag in getting title prefix

---
+++ test/server/controllers/article-helpers/tag-title-prefix.test.js
@@ -0,0 +1,33 @@
+/*global describe, it, before*/
+
+'use strict';
+
+const expect = require('chai').expect;
+const tagTitlePrefix = require('../../../../server/controllers/article-helpers/tag-title-prefix');
+
+describe('Tag Title Prefix', () => {
+
+	context('When tag exists', () => {
+
+		const tagAuthor = {taxonomy: 'authors'};
+		const tagSection = {taxonomy: 'sections'};
+		const tagGenre = {taxonomy: 'genre'};
+		const tagOther = {taxonomy: 'other'};
+
+		it('translates the taxonomy into a title prefix', () => {
+			expect(tagTitlePrefix(tagAuthor).title).to.equal('from');
+			expect(tagTitlePrefix(tagSection).title).to.equal('in');
+			expect(tagTitlePrefix(tagGenre).title).to.equal('');
+			expect(tagTitlePrefix(tagOther).title).to.equal('on');
+		});
+
+	});
+
+	context('When no tag', () => {
+
+		it('does not break', () => {
+			expect(tagTitlePrefix(undefined)).to.be.undefined;
+		});
+
+	});
+});
