50ca7e9f305379f734c9664ea07f734f85e014d9
Matt Chadburn <matt@commuterjoy.co.uk>
2016-03-24 10:44:25 +0000

    Put the access controller back in

--- server/app.js
+++ server/app.js
@@ -24,6 +24,8 @@ app.post('^/preview$', require('./controllers/preview'));
 
 const uuid = '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}';
 
+app.use(`^/content/:id(${uuid})$`, require('./controllers/access'));
+
 app.get('^/article/:id/story-package', require('./controllers/related/story-package'));
 app.get('^/article/:id/more-on', require('./controllers/related/more-on'));
 app.get('^/article/:id/special-report', require('./controllers/related/special-report'));
