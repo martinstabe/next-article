74adf11f5578204109acd99fb9d0340d7f4a2b7b
Arjun Gadhia <arjungadhia@gmail.com>
2016-03-29 15:26:27 +0100

    Simplify ad slot config

--- server/transforms/inline-ad.js
+++ server/transforms/inline-ad.js
@@ -6,7 +6,7 @@ module.exports = function ($) {
 	const pars = $('p');
 	pars.each((index, par) => {
 		if(index > 1 && par.next && par.next.name === 'p') {
-			$(par).after('<div class="o-ads advertising__article-text" data-o-ads-name="mpu" data-o-ads-center="true" data-o-ads-label="true" data-o-ads-targeting="pos=mpu;" data-o-ads-formats-default="MediumRectangle,Responsive" data-o-ads-formats-small="MediumRectangle,Responsive" data-o-ads-formats-medium="MediumRectangle,Responsive" data-o-ads-formats-large="false" data-o-ads-formats-extra="false" aria-hidden="true"></div>');
+			$(par).after('<div class="o-ads advertising__article-text" data-o-ads-name="mpu-in-article" aria-hidden="true"></div>');
 			return false;
 		}
 	});
