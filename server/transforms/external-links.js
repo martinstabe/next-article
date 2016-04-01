ea56d70b326fc30da10ebd2875785ea6770be94b
andygnewman <andrew.newman@ft.com>
2016-04-01 13:56:41 +0100

    open external links in new tab

---
+++ server/transforms/external-links.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function($) {
+
+	const $links = $('a');
+	const httpMatcher = /^https?:\/\//;
+	const ftMatcher = /.ft.com\//;
+
+	$links.each(i => {
+		const $thisLink = $links.eq(i);
+
+		if (httpMatcher.test($thisLink.attr('href')) &&
+				!ftMatcher.test($thisLink.attr('href'))) {
+			$thisLink.attr('target', '_blank');
+		}
+	});
+
+	return $;
+
+}
