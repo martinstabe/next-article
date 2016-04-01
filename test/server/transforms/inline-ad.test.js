c4f066db999fff71070f1e0954adc9565ee11cf4
Arjun Gadhia <arjungadhia@gmail.com>
2016-04-01 16:28:39 +0100

    Fix unit test

--- test/server/transforms/inline-ad.test.js
+++ test/server/transforms/inline-ad.test.js
@@ -6,7 +6,7 @@ var inlineAdTransform = require('../../../server/transforms/inline-ad');
 
 describe('Inline ad inside body', function () {
 
-	const adHtml = '<div class="o-ads advertising__article-text" data-o-ads-name="mpu" data-o-ads-center="true" data-o-ads-label="true" data-o-ads-targeting="pos=mpu;" data-o-ads-formats-default="MediumRectangle,Responsive" data-o-ads-formats-small="MediumRectangle,Responsive" data-o-ads-formats-medium="MediumRectangle,Responsive" data-o-ads-formats-large="false" data-o-ads-formats-extra="false" aria-hidden="true"></div>';
+	const adHtml = '<div class="o-ads in-article-advert" data-o-ads-name="mpu" data-o-ads-center="true" data-o-ads-label="true" data-o-ads-targeting="pos=mpu;" data-o-ads-formats-default="MediumRectangle,Responsive" data-o-ads-formats-small="MediumRectangle,Responsive" data-o-ads-formats-medium="MediumRectangle,Responsive" data-o-ads-formats-large="Responsive" data-o-ads-formats-extra="Responsive" aria-hidden="true"></div>';
 
 	it('should insert an ad between the third and fourth paragraphs', function() {
 		var $ = cheerio.load('<p>1</p><p>2</p><p>3</p><p>4</p>');
