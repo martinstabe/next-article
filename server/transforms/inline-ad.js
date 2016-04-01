d77a5c4362abc615e3c9e19fb67cc6b2cca4fa83
Arjun Gadhia <arjungadhia@gmail.com>
2016-04-01 16:06:56 +0100

    Use full ad slot config

--- server/transforms/inline-ad.js
+++ server/transforms/inline-ad.js
@@ -6,7 +6,17 @@ module.exports = function ($) {
 	const pars = $('p');
 	pars.each((index, par) => {
 		if(index > 1 && par.next && par.next.name === 'p') {
-			$(par).after('<div class="o-ads in-article-advert" data-o-ads-name="mpu-in-article" data-o-ads-targeting="pos=mpu;" aria-hidden="true"></div>');
+			$(par).after(`<div class="o-ads in-article-advert"
+				data-o-ads-name="mpu"
+				data-o-ads-center="true"
+				data-o-ads-label="true"
+				data-o-ads-targeting=${"pos=mpu;"}
+				data-o-ads-formats-default="MediumRectangle,Responsive"
+				data-o-ads-formats-small="MediumRectangle,Responsive"
+				data-o-ads-formats-medium="MediumRectangle,Responsive"
+				data-o-ads-formats-large="Responsive"
+				data-o-ads-formats-extra="Responsive"
+				aria-hidden="true"></div>`);
 			return false;
 		}
 	});
