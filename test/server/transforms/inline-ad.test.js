95dde030c6a907fb65e2219ab0a25948a99dd549
Arjun Gadhia <arjungadhia@gmail.com>
2016-04-07 11:37:01 +0100

    Update inline ad test

--- test/server/transforms/inline-ad.test.js
+++ test/server/transforms/inline-ad.test.js
@@ -10,20 +10,26 @@ describe('Inline ad inside body', function () {
 
 	it('should insert an ad between the third and fourth paragraphs', function() {
 		var $ = cheerio.load('<p>1</p><p>2</p><p>3</p><p>4</p>');
-		$ = inlineAdTransform($);
+		$ = inlineAdTransform($, {}, 'default');
 		expect($.html()).to.equal(`<p>1</p><p>2</p><p>3</p>${adHtml}<p>4</p>`);
 	});
 
 	it('should not insert an ad if there are only three paragraphs', function() {
 		var $ = cheerio.load('<p>1</p><img><p>2</p><p>3</p><img>');
-		$ = inlineAdTransform($);
+		$ = inlineAdTransform($, {}, 'default');
 		expect($.html()).to.equal(`<p>1</p><img><p>2</p><p>3</p><img>`);
 	});
 
 	it('should move the ad later if there is any other element after the third paragraph', function() {
 		var $ = cheerio.load('<p>1</p><img><p>2</p><p>3</p><img><p>4</p><p>5</p>');
-		$ = inlineAdTransform($);
+		$ = inlineAdTransform($, {}, 'default');
 		expect($.html()).to.equal(`<p>1</p><img><p>2</p><p>3</p><img><p>4</p>${adHtml}<p>5</p>`);
+	});
+
+	it('should call the targeting pos "mid" for the new layout', function() {
+		var $ = cheerio.load('<p>1</p><img><p>2</p><p>3</p><img><p>4</p><p>5</p>');
+		$ = inlineAdTransform($, {}, 'responsive');
+		expect($.html()).to.equal(`<p>1</p><img><p>2</p><p>3</p><img><p>4</p>${adHtml.replace("pos=mpu;", "pos=mid;")}<p>5</p>`);
 	});
 
 });
