b309ab7ab92417a499c277c294d120aa3052c3dc
TheoLeanse <theoleanse@gmail.com>
2016-04-11 18:02:48 +0100

    add new copy

--- server/transforms/light-sign-up.js
+++ server/transforms/light-sign-up.js
@@ -8,10 +8,10 @@ module.exports = function ($, flags, adsLayout) {
 
 	<div class="o-grid-row o-grid-row--compact">
 		<div data-o-grid-colspan="12">
-			<div class="n-light-signup__close">close</div>
-			<h2 class="n-light-signup__heading">Want a summary of the top stories of the day?</h2>
+			<div class="n-light-signup__close"></div>
+			<h2 class="n-light-signup__heading">Get a free taste of the FT</h2>
 			<p class="n-light-signup__tagline">
-				We are currently trialling and collecting feedback on our daily top stories summary email. Please enter your email address below to be among the first to receive the email that contains 3 free articles, for 7 days.
+				Sample the FT, with the top stories sent FREE to your inbox every morning. For 7 days, you’ll get an email with free access to the 3 most read FT stories.
 			</p>
 		</div>
 	</div>
@@ -20,53 +20,17 @@ module.exports = function ($, flags, adsLayout) {
 
 		<div class="o-grid-row o-grid-row--compact">
 			<div data-o-grid-colspan="12">
-
-				<p class="n-light-signup__checkbox-group o-forms-group">
-					<input type="checkbox" name="terms" id="checkbox" value="terms" class="o-forms-checkbox"/>
-					<label class="o-forms-label" for="checkbox">By ticking this checkbox you agree to our <a href="http://help.ft.com/tools-services/ft-com-terms-and-conditions">terms and conditions</a>, <a href="http://help.ft.com/tools-services/how-the-ft-manages-cookies-on-its-websites">cookie policy</a> and <a href="http://help.ft.com/tools-services/financial-times-privacy-policy">privacy policy</a>.</label>
-					<div class="o-forms-errortext n-light-signup__consent-error-msg n-light-signup__visually-hidden">You must accept the T&Cs to proceed.</div>
-				</p>
-
+				<form class="n-light-signup__form">
+					<label for="email" class="o-forms-label n-light-signup__visually-hidden">Email address:</label>
+					<input type="text" id="email" placeholder="your@email.com" name="email" class="n-light-signup__email o-forms-text" data-trackable="email"/>
+					<div class="o-forms-errortext n-light-signup__email-error-msg n-light-signup__visually-hidden">Invalid email</div>
+					<p>By clicking submit you confirm that you have read and agree to the <a href="http://help.ft.com/tools-services/ft-com-terms-and-conditions">terms and conditions</a>, <a href="http://help.ft.com/tools-services/how-the-ft-manages-cookies-on-its-websites">cookie policy</a> and <a href="http://help.ft.com/tools-services/financial-times-privacy-policy">privacy policy</a>.</p>
+					<input type="submit" value="Sign up" class="n-light-signup__submit" data-trackable="submit"/>
+					<p class="n-light-signup__no-spam">Unsubscribe at any time. <a href="#" class="n-light-signup__link" data-trackable="see-sample-email">See sample email</a>.</p>
+				</form>
 			</div>
 		</div>
 
-		<div class="o-grid-row o-grid-row--compact">
-			<div data-o-grid-colspan="12">
-
-				<div class="o-grid-row o-grid-row--compact"
-					<div class="n-light-signup__email-group o-forms-group">
-
-						<div data-o-grid-colspan="10">
-							<label for="email" class="o-forms-label n-light-signup__visually-hidden">Email address:</label>
-							<input type="text" id="email" placeholder="your@email.com" name="email" class="n-light-signup__email o-forms-text" data-trackable="email"/>
-							<div class="o-forms-errortext n-light-signup__email-error-msg n-light-signup__visually-hidden">Invalid email</div>
-						</div>
-
-						<div data-o-grid-colspan="2">
-							<input type="submit" value="Subscribe" class="n-light-signup__submit" data-trackable="submit"/>
-						</div>
-
-					</div>
-				</div>
-
-			</div>
-		</div>
-
-		<div class="o-grid-row o-grid-row--compact">
-			<div data-o-grid-colspan="12">
-
-				<p class="n-light-signup__no-spam">
-					No spam. One-click unsubscribe. <a href="#" class="n-light-signup__link" data-trackable="see-sample-email">See sample email</a>.
-				</p>
-
-			</div>
-		</div>
-
-	</div>
-
-	<div class="n-light-signup__thank-you" style="display:none;">
-		<p class="n-light-signup__heading">Thank you for signing up!</p>
-		<p>We will send you a summary of the top stories later this afternoon.</p>
 	</div>
 
 </div>
