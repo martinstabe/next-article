1b86d190d6ef536e31d49555d3516469bd9a8f90
TheoLeanse <theoleanse@gmail.com>
2016-04-12 16:06:14 +0100

    test the light signup transform

--- server/transforms/light-sign-up.js
+++ server/transforms/light-sign-up.js
@@ -18,10 +18,10 @@ module.exports = function ($, flags) {
 						<div class="o-grid-row o-grid-row--compact">
 							<div data-o-grid-colspan="12">
 								<form class="n-light-signup__form">
-									<input type="text" id="email" placeholder="your@email.com" name="email" class="n-light-signup__email o-forms-text" data-trackable="email"/>
+									<input type="text" id="email" placeholder="your@email.com" name="email" class="n-light-signup__email o-forms-text" data-trackable="email">
 									<div class="o-forms-errortext n-light-signup__email-error-msg n-light-signup__visually-hidden">Invalid email</div>
 									<p>By clicking submit you confirm that you have read and agree to the <a href="http://help.ft.com/tools-services/ft-com-terms-and-conditions">terms and conditions</a>, <a href="http://help.ft.com/tools-services/how-the-ft-manages-cookies-on-its-websites">cookie policy</a> and <a href="http://help.ft.com/tools-services/financial-times-privacy-policy">privacy policy</a>.</p>
-									<input type="submit" value="Sign up" class="n-light-signup__submit" data-trackable="submit"/>
+									<input type="submit" value="Sign up" class="n-light-signup__submit" data-trackable="submit">
 									<p class="n-light-signup__no-spam">Unsubscribe at any time. <a href="#" class="n-light-signup__link" data-trackable="see-sample-email">See sample email</a>.</p>
 								</form>
 							</div>
