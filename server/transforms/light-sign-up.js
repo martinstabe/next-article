73f4c7a661b5f73e5594499a7331a19a903fe512
TheoLeanse <theoleanse@gmail.com>
2016-04-13 15:02:35 +0100

    just add a placeholder for the future component

--- server/transforms/light-sign-up.js
+++ server/transforms/light-sign-up.js
@@ -4,26 +4,7 @@ module.exports = function ($, flags) {
 	const pars = $('p');
 	pars.each((index, par) => {
 		if(index > 3 && par.next && par.next.name === 'p') {
-			$(par).after(`<div class="n-light-signup" data-trackable="light-signup">
-
-							<div class="n-light-signup__close"></div>
-							<h2 class="n-light-signup__heading">Get a free taste of the FT</h2>
-							<p class="n-light-signup__tagline">
-								Sample the FT, with the top stories sent FREE to your inbox every morning. For 7 days, you’ll get an email with free access to the 3 most read FT stories.
-							</p>
-
-					<div class="n-light-signup__secondary">
-
-								<form class="n-light-signup__form">
-									<input type="text" id="email" placeholder="your@email.com" name="email" class="n-light-signup__email o-forms-text" data-trackable="email">
-									<div class="o-forms-errortext n-light-signup__email-error-msg n-light-signup__visually-hidden">Invalid email</div>
-									<p>By clicking submit you confirm that you have read and agree to the <a href="http://help.ft.com/tools-services/ft-com-terms-and-conditions">terms and conditions</a>, <a href="http://help.ft.com/tools-services/how-the-ft-manages-cookies-on-its-websites">cookie policy</a> and <a href="http://help.ft.com/tools-services/financial-times-privacy-policy">privacy policy</a>.</p>
-									<input type="submit" value="Sign up" class="n-light-signup__submit" data-trackable="submit">
-									<p class="n-light-signup__no-spam">Unsubscribe at any time. <a href="#" class="n-light-signup__link" data-trackable="see-sample-email">See sample email</a>.</p>
-								</form>
-
-					</div>
-				</div>`);
+			$(par).after('<div class="n-light-signup__container"></div>');
 			return false;
 		}
 	});
