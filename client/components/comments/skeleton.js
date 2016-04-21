673efc2a53fadaad16dd3d20831f553624c55639
Theo <theoleanse@gmail.com>
2016-04-21 11:26:20 +0100

    Light signup mvt test (#1327)
    * add and test alternate component placement
    * don't add component in a pullquote or aside

--- client/components/comments/skeleton.js
+++ client/components/comments/skeleton.js
@@ -4,25 +4,21 @@ const createSkeleton = {
 	init: function() {
 		const skeleton = document.createElement('div');
 		const skeletonInner = document.createElement('div');
+		const skeletonAvatar = document.createElement('div');
 		const skeletonPod = document.createElement('div');
-		const skeletonEdit = document.createElement('div');
-		const skeletonFollow = document.createElement('div');
-		const skeletonSubmit = document.createElement('div');
 		const commentsEl = document.querySelector('#comments');
 
 		skeleton.className = 'skeleton';
 		skeleton.setAttribute('data-skeleton', 'comments');
 		skeletonInner.className = 'skeleton__inner';
+		skeletonAvatar.className = 'skeleton__avatar';
 		skeletonPod.className = 'skeleton__text';
-		skeletonEdit.className = 'skeleton__edit';
-		skeletonFollow.className = 'skeleton__follow';
-		skeletonSubmit.className = 'skeleton__submit';
 
-		skeleton.appendChild(skeletonInner);
+		skeletonInner.appendChild(skeletonAvatar);
 		skeletonInner.appendChild(skeletonPod);
-		skeletonInner.appendChild(skeletonEdit);
-		skeletonInner.appendChild(skeletonFollow);
-		skeletonInner.appendChild(skeletonSubmit);
+		skeleton.innerHTML += skeletonInner.outerHTML;
+		skeleton.innerHTML += skeletonInner.outerHTML;
+		skeleton.innerHTML += skeletonInner.outerHTML;
 		commentsEl.parentNode.insertBefore(skeleton, commentsEl.nextSibling);
 	}
 }
