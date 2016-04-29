1e377a355a807b28011ea6ace395ced976e67798
Sean Tracey <seanmtracey@gmail.com>
2016-04-29 10:36:12 +0100

    Chalkboard ad-block-handling demo. Not active atm, as content is not put back in the page correctly

---
+++ client/components/ftlabsadblockerhandling/lib/content-hider.js
@@ -0,0 +1,32 @@
+let contentElement = undefined
+let originalContent = undefined;
+let parentEl = undefined;
+
+function setElement (element){
+	
+	if(contentElement !== undefined || originalContent !== undefined || parentEl !== undefined){
+		return false;
+	}
+	
+	contentElement = element;
+	parentEl = element.parentNode;
+	originalContent = element.cloneNode(true);
+
+}
+
+function hideContent (){
+	contentElement.innerHTML = "";
+}
+
+function showContent (){
+	parentEl.removeChild(contentElement);
+	parentEl.appendChild(originalContent);
+}
+
+module.exports = function (){
+	return {
+		set : setElement,
+		hide : hideContent,
+		show : showContent	
+	};			
+}
