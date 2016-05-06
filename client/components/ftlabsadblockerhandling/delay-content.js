121462660621cc1684bd3b259e0eac64b4bd75f4
andygnewman <andrew.newman@ft.com>
2016-05-06 19:10:05 +0100

    ad blocking response delayed content message styling

--- client/components/ftlabsadblockerhandling/delay-content.js
+++ client/components/ftlabsadblockerhandling/delay-content.js
@@ -10,7 +10,7 @@ module.exports = {
 		articleParas.map(el => el.style.display = 'none');
 
 		const adMessage = document.createElement('div');
-		adMessage.innerHTML = '<p>This article will be back shortly. We have delayed your access to it for a short while because we believe you are using ad blocking technology. The FT generates income from advertising on the site which helps ensure we can bring you intesting content like the article you will shortly be reading.</p>';
+		adMessage.innerHTML = '<p class="ftlabs-ad-block__delay-content">This article will be back shortly. We have delayed your access to it for a short while because we believe you are using ad blocking technology. The FT generates income from advertising on the site which helps ensure we can bring you intesting content like the article you will shortly be reading.</p>';
 		articleParent.insertBefore(adMessage, articleParent.firstChild);
 
 		function showContent() {
