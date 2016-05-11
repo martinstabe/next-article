32861a1756916e2d5e8f34ec7f471c273484758f
Adam Tavener <adam.tavener@ft.com>
2016-05-11 12:52:22 +0100

    Add mailing list targeting to the light signup form (#1376)
    Surfaces section data on the light signup component so that users
    can be signed up to a specific mailing list. Tweaks the transform
    function and adds new tests. Passes new additional data
    from article.js via the option object to the transfrom.

--- server/transforms/light-sign-up.js
+++ server/transforms/light-sign-up.js
@@ -5,6 +5,15 @@ module.exports = function ($, flags, options) {
 
 	const pars = $('p');
 	const variant = flags.lightSignUp;
+	const sectionData = options.metadata.primarySection || null;
+	let mailingList = {
+		name: 'default',
+		prefLabel: 'default'
+	}
+
+	if (sectionData && sectionData.name && sectionData.prefLabel) {
+		mailingList = Object.assign(mailingList, sectionData);
+	}
 
 	if (variant === 'top') positionComponent(1, true);
 	if (variant === 'mid') positionComponent(5, true);
@@ -12,13 +21,13 @@ module.exports = function ($, flags, options) {
 
 	return $;
 
-	function positionComponent(position, checkNextP) {
+	function positionComponent (position, checkNextP) {
 		pars.each((index, par) => {
 			let indexMatches = ((index + 1) >= position);
 			let isOrphan = !par.parent;
 			let hasNextP = (par.next && par.next.name === 'p');
 			if (indexMatches && isOrphan && (hasNextP || !checkNextP)) {
-				$(par).after('<div class="n-light-signup__container"></div>');
+				$(par).after(`<div class="n-light-signup__container" data-n-light-signup-list-name="${mailingList.name}" data-n-light-signup-list-pref-label="${mailingList.prefLabel}"></div>`);
 				return false;
 			}
 		});
