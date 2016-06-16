21e0e476084f06aebe99e7c49c6deebe4de0a19f
Rhys Evans <wheresrhys@gmail.com>
2016-06-16 16:30:10 +0100

    updated to n-webpack (#1437)

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const nWebpack = require('@financial-times/n-webpack');
+
+const config = require('./n-makefile');
+
+module.exports = nWebpack({
+	withHeadCss: true,
+	withHashedAssets: true,
+	withBabelPolyfills: true,
+	entry: config.assets.entry
+});
