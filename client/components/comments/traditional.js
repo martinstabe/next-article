89c6acf6dbf04379db897bcfbdcb2312d4d5f4a9
George Crawford <g.o.crawford@gmail.com>
2016-07-07 10:50:19 +0100

    Remove duplicate comments tracking (#1459)
    Back in the days of iJento, we subscribed to o-comments events and sent tracking calls to record them. In the meantime, tracking has migrated to Spoor, but o-comments has also implemented its own tracking internally. Next-article therefore sends two events to Spoor: `page:comment` and `comment:post`, for example. This is unnecessary.

--- client/components/comments/traditional.js
+++ client/components/comments/traditional.js
@@ -1,6 +1,5 @@
 'use strict';
 const OComments = require('o-comments');
-const trackEvent = require('../utils/tracking');
 
 module.exports = {};
 module.exports.init = function(uuid, flags) {
@@ -14,31 +13,9 @@ module.exports.init = function(uuid, flags) {
 		return;
 	}
 
-	const eventData = {
-		action: 'comment',
-		category: 'page',
-		context: {
-			product: 'next',
-			source: 'next-article'
-		}
-	};
-
-
 	OComments.on('widget.renderComplete', function () {
 		const skeleton = document.querySelector('[data-skeleton=comments]');
 		skeleton.parentNode.removeChild(skeleton);
-	});
-	OComments.on('tracking.postComment', function () {
-		eventData.meta = { interaction: 'posted' };
-		trackEvent(eventData);
-	});
-	OComments.on('tracking.likeComment', function (ev) {
-		eventData.meta = { interaction: 'liked', id: ev.detail.data.lfEventData.targetId };
-		trackEvent(eventData);
-	});
-	OComments.on('tracking.shareComment', function (ev) {
-		eventData.meta = { interaction: 'shared', id: ev.detail.data.lfEventData.targetId };
-		trackEvent(eventData);
 	});
 
 	new OComments(document.querySelector('#comments'), {
