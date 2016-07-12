8d1a26efd599dbb24a777346aac0c38216213808
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-07-12 15:03:31 +0100

    Add a spec for the IG poller module

--- server/lib/ig-poller.js
+++ server/lib/ig-poller.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var pollerData = [];
-var Poller = require('ft-poller');
-var poller = new Poller({
-	url: 'https://bertha.ig.ft.com/view/publish/gss/1Rlx_BfkwCpE8KBJZVK_p5gO61HZF7fRckQige6Hwbl8/mappings',
-	parseData: function (data) {
-		pollerData = data;
-	}
+const Poller = require('ft-poller');
+
+const poller = new Poller({
+	url: 'https://bertha.ig.ft.com/view/publish/gss/1Rlx_BfkwCpE8KBJZVK_p5gO61HZF7fRckQige6Hwbl8/mappings'
 });
 
-module.exports = {
-	start: poller.start.bind(poller, { initialRequest: true }),
-	getData: function () {
-		return pollerData;
-	}
-};
+function start () {
+	return poller.start({ initialRequest: true });
+}
+
+function getData () {
+	return poller.getData();
+}
+
+module.exports = { start, getData };
