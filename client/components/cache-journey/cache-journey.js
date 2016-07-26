23727475e09558655cb36dd334b7ba167bf3e8c5
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-07-26 10:23:16 +0100

    Only cache a single article

--- client/components/cache-journey/cache-journey.js
+++ client/components/cache-journey/cache-journey.js
@@ -2,7 +2,7 @@ import * as serviceWorker from 'n-service-worker';
 
 export default () => {
 	// grab the articles most likely to make the onward journey
-	const onwardArticles = [...document.querySelectorAll('.next-up .next-up__headline')];
+	const onwardArticles = [...document.querySelectorAll('.next-up__header .next-up__headline')];
 	serviceWorker
 		.message({
 			type: 'cacheContent',
