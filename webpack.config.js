4236266d7e808b45990ffe66e1c3b339ca366389
Rhys Evans <wheresrhys@gmail.com>
2016-08-10 12:07:11 +0100

    update n-webpack (#1499)

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,13 @@
 const nWebpack = require('@financial-times/n-webpack');
 
-module.exports = [
-	nWebpack({
-		withHeadCss: true,
-		withHashedAssets: true,
-		withBabelPolyfills: true,
-		entry: {
-			'./public/main.js': './client/main.js',
-			'./public/main.css': './client/main.scss',
-			'./public/main-ie8.css': './client/main-ie8.scss',
-			'./public/comments.js': './client/comments.js',
-			'./public/comments.css': './client/comments.scss'
-		}
-	}),
-	nWebpack({
-		withHashedAssets: true,
-		withBabelPolyfills: false,
-		externals: {'n-ui': true},
-		entry: {
-			'./public/main-without-n-ui.js': './client/main.js'
-		}
-	})
-];
+module.exports = nWebpack({
+	withHeadCss: true,
+	withHashedAssets: true,
+	withBabelPolyfills: true,
+	entry: {
+		'./public/main.js': './client/main.js',
+		'./public/main.css': './client/main.scss',
+		'./public/comments.js': './client/comments.js',
+		'./public/comments.css': './client/comments.scss'
+	}
+}, true)
