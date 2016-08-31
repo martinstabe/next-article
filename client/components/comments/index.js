8ee770a77792741752128c065f7b17f67dd8344d
Rhys Evans <wheresrhys@gmail.com>
2016-08-31 14:55:15 +0100

    bug fixes for comment tracking

--- client/components/comments/index.js
+++ client/components/comments/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-import {broadcast} from 'n-ui/utils';
-import superstore from 'superstore-sync';
+import tracking from './tracking';
 
 const commentsIcon = require('./icon');
 const commentsSkeleton = require('./skeleton');
@@ -32,7 +31,7 @@ const intersectionCallback = (observer, changes, sources, resolve) => {
 };
 
 function lazyLoad (opts) {
-	const rumIndicatorEl = document.querySelector('.comments__rum-indicator');
+
 	return new Promise((resolve, reject) => {
 		const target = document.querySelector(opts.targetEl);
 		if (target) {
@@ -46,28 +45,6 @@ function lazyLoad (opts) {
 					{ rootMargin: `${opts.threshold}px` }
 				);
 				observer.observe(target);
-				const rumObserver = new IntersectionObserver(
-					() => {
-						window.FT.commentsRumInView = Date.now();
-						startingCommentEngagement
-							.then(score => {
-								broadcast('oTracking.event', {
-									action: 'view',
-									category: 'comments',
-									context: {
-										product: 'next',
-										source: 'next-article',
-										uiIsDelayed: !window.FT.commentsRumLoaded,
-										userCommentEngagement: score
-									}
-								});
-							})
-						logCommentEngagement('passive')
-						rumObserver.unobserve(rumIndicatorEl);
-					},
-					{ rootMargin: `0px` }
-				);
-				rumObserver.observe(rumIndicatorEl);
 			} else {
 				loadSources(opts.sources, resolve);
 			}
@@ -77,54 +54,8 @@ function lazyLoad (opts) {
 	});
 }
 
-const engagementKey = 'next.comments.engagement';
-
-function isWithinNDays (timestamp, n) {
-	return (Date.now() - timestamp) < (1000 * 60 * 60 * 24 * n)
-}
-
-function getCommentEngagement () {
-	const value = superstore.get(engagementKey)
-	if (!value) {
-		return 'none';
-	}
-
-	value = JSON.parse(value);
-
-	// if has actively sought aout/interacted with comments in the last 30 days
-	if (value.active && isWithinNDays(value.active, 30)) {
-		return 'active';
-	}
-
-	if (value.passive) {
-		// if scrolled as far as comments once in the last 5 days or 4 times in the last month
-		if (isWithinNDays(value.passive[0], 5) ||	value.passive.filter(timestamp => isWithinNDays(timestamp, 30)).length > 3) {
-			return 'passive';
-		}
-	}
-
-	return 'previous';
-}
-
-function logCommentEngagement (type) {
-	let value = superstore.get(engagementKey)
-	value = value ? JSON.parse(value) : {active: null, passive: []};
-	if (type === 'active') {
-		value.active = Date.now();
-	} else {
-		value.passive.unshift(Date.now())
-		if (value.passive.length > 10) {
-			value.passive.pop();
-		}
-	}
-	superstore.set(engagementKey, JSON.stringify(value.slice(0, 5)))
-}
-
-let startingCommentEngagement;
-
 module.exports = {
 	init: () => {
-		startingCommentEngagement = getCommentEngagement();
 
 		window.FT = window.FT || {};
 		const commentsEl = document.getElementById('comments');
@@ -142,29 +73,6 @@ module.exports = {
 			commentsLazyLoad
 		});
 
-		document.body.addEventListener('oComments.widget.renderComplete', () => {
-			window.FT.commentsRumLoaded = Date.now();
-			// TODO - record number of comments fetched
-			broadcast('oTracking.event', {
-				category: 'comments',
-				action: 'ready',
-				context: {
-					timeToLoad: window.FT.commentsRumLoaded - window.FT.commentsRumLazyStart,
-					userIsViewing: !!window.FT.commentsRumInView,
-					timeUserWaitedToView: window.FT.commentsRumInView ? window.FT.commentsRumLoaded - window.FT.commentsRumInView : 0
-				}
-			});
-		})
-
-		Array.from(document.querySelectorAll('[href="#comments"]')).forEach(el => {
-			el.addEventListener('click', () => logCommentEngagement('active'));
-		})
-
-		document.body.addEventListener('oComments.tracking.postComment', () logCommentEngagement('active'));
-
-		document.body.addEventListener('oComments.tracking.likeComment', () logCommentEngagement('active'));
-
-		document.body.addEventListener('oComments.tracking.shareComment', () logCommentEngagement('active'));
-
+		tracking.init();
 	}
 };
---
+++ client/components/comments/tracking.js
@@ -0,0 +1,100 @@
+'use strict';
+import {broadcast} from 'n-ui/utils';
+const superstore = require('superstore-sync');
+
+const engagementKey = 'next.comments.engagement';
+let initialCommentEngagement;
+
+function isWithinNDays (timestamp, n) {
+	return (Date.now() - timestamp) < (1000 * 60 * 60 * 24 * n)
+}
+
+function getCommentEngagement () {
+	let value = superstore.local.get(engagementKey)
+	if (!value) {
+		return 'none';
+	}
+
+	// if has actively sought aout/interacted with comments in the last 30 days
+	if (value.active && isWithinNDays(value.active, 30)) {
+		return 'active';
+	}
+
+	if (value.passive) {
+		// if scrolled as far as comments once in the last 5 days or 4 times in the last month
+		if (isWithinNDays(value.passive[0], 5) ||	value.passive.filter(timestamp => isWithinNDays(timestamp, 30)).length > 3) {
+			return 'passive';
+		}
+	}
+
+	return 'previous';
+}
+
+function logCommentEngagement (type) {
+	let value = superstore.local.get(engagementKey)
+	value = value || {active: null, passive: []};
+	if (type === 'active') {
+		value.active = Date.now();
+	} else {
+		value.passive.unshift(Date.now())
+		if (value.passive.length > 10) {
+			value.passive = value.passive.slice(0, 10);
+		}
+	}
+	superstore.local.set(engagementKey, value)
+}
+
+
+module.exports = {
+	init: () => {
+		initialCommentEngagement = getCommentEngagement();
+
+		const rumIndicatorEl = document.querySelector('.comments__rum-indicator');
+		const rumObserver = new IntersectionObserver(
+			() => {
+				window.FT.commentsRumInView = Date.now();
+
+				broadcast('oTracking.event', {
+					action: 'view',
+					category: 'comments',
+					context: {
+						product: 'next',
+						source: 'next-article',
+						uiIsDelayed: !window.FT.commentsRumLoaded,
+						userEngagement: initialCommentEngagement
+					}
+				});
+				logCommentEngagement('passive')
+				rumObserver.unobserve(rumIndicatorEl);
+			},
+			{ rootMargin: `0px` }
+		);
+		rumObserver.observe(rumIndicatorEl);
+
+		document.body.addEventListener('oComments.widget.renderComplete', () => {
+			window.FT.commentsRumLoaded = Date.now();
+			// TODO - record number of comments fetched
+			broadcast('oTracking.event', {
+				category: 'comments',
+				action: 'ready',
+				context: {
+					timeToLoad: window.FT.commentsRumLoaded - window.FT.commentsRumLazyStart,
+					userIsViewing: !!window.FT.commentsRumInView,
+					timeUserWaitedToView: window.FT.commentsRumInView ? window.FT.commentsRumLoaded - window.FT.commentsRumInView : 0,
+					userEngagement: initialCommentEngagement
+				}
+			});
+		})
+
+		Array.from(document.querySelectorAll('[href="#comments"]')).forEach(el => {
+			el.addEventListener('click', () => logCommentEngagement('active'));
+		})
+
+		document.body.addEventListener('oComments.tracking.postComment', () => logCommentEngagement('active'));
+
+		document.body.addEventListener('oComments.tracking.likeComment', () => logCommentEngagement('active'));
+
+		document.body.addEventListener('oComments.tracking.shareComment', () => logCommentEngagement('active'));
+
+	}
+};
