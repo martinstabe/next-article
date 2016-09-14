7df9cc4282d21c18c7737dd873130bcdc730c9dd
Arjun Gadhia <arjungadhia@gmail.com>
2016-09-14 15:13:15 +0100

    Update Smartmatch impression tracking and creative ID

--- client/components/ads/promoted-content.js
+++ client/components/ads/promoted-content.js
@@ -14,14 +14,14 @@ function correlator(len) {
 	return (new Date().getTime() + "" + genRand(16) + Math.random().toString(34).slice(2)).toString().substr(0, len);
 }
 
-const getSmartmatchData = (adUnit) => {
+const getSmartmatchData = (adUnit, dfpResponse) => {
 	const uuid = document.documentElement.getAttribute('data-content-id');
 
 	//Smartmatch explicitly require this to be double encoded
 	const contentUrl = encodeURIComponent(encodeURIComponent(`ft.com/content/${uuid}`));
 	const splitAdUnit = adUnit.split('/');
 	const section = splitAdUnit[splitAdUnit.length - 1];
-	const url = `https://c.smartonomi.net/static/creative/604/key/i0t0hprwvhc5/pcid/${contentUrl}/sct=${encodeURIComponent(section)};encode=false;?cacheBuster=${correlator(64)}`
+	const url = `https://c.smartonomi.net/static/creative/${dfpResponse.creativeID}/key/${dfpResponse.smartmatchKey}/pcid/${contentUrl}/sct=${encodeURIComponent(section)};encode=false;?cacheBuster=${correlator(64)}`
 	return crossDomainFetch(url, {
 		timeout: 4000,
 		mode: 'cors'
@@ -33,6 +33,8 @@ const getSmartmatchData = (adUnit) => {
 	})
 	.then(data => {
 		if(data && data.type && data.title) {
+			//add the DFP impression URL to smartmatch impression URLS
+			data.impressionURL = [].concat(data.impressionURL, dfpResponse.impressionURL);
 			return data;
 		} else {
 			throw 'No smartmatch results';
@@ -102,7 +104,7 @@ function initPaidPost(el, flags, ads, skipSmartmatch) {
 	const adTargeting = ads.targeting.get();
 	const custParams = Object.keys(adTargeting).map(k => k + '=' + encodeURIComponent(adTargeting[k])).join('&');
 	const adUnit = window.oAds.config('gpt').unitName || '5887/ft.com/home/UK';
-	let url = `https://securepubads.g.doubleclick.net/gampad/ads?gdfp_req=1&correlator=${correlator()}&output=json_html&impl=fif&sc=1&sfv=1-0-4&iu=%2F5887%2F${adUnit.replace(/\/?5887\//, '')}&sz=320x50&fluid=height&scp=${encodeURIComponent(slotParams)}&ga_sid=1469788770&cust_params=${encodeURIComponent(custParams)}`;
+	let url = `https://securepubads.g.doubleclick.net/gampad/ads?gdfp_req=1&correlator=${correlator()}&output=json_html&impl=fif&sc=1&sfv=1-0-4&iu=%2F5887%2F${adUnit.replace(/\/?5887\//, '')}&sz=320x50&fluid=height&scp=${encodeURIComponent(slotParams)}&d_imp=1&ga_sid=${new Date().getTime()}&cust_params=${encodeURIComponent(custParams)}`;
 
 	if(skipSmartmatch) {
 		url += encodeURIComponent('&ftpb=1');
@@ -131,7 +133,7 @@ function initPaidPost(el, flags, ads, skipSmartmatch) {
 				initPaidPost(secondEl, flags, ads);
 			}
 		} else if (data && data.type === 'smartmatch') {
-			getSmartmatchData(adUnit)
+			getSmartmatchData(adUnit, data)
 			.then(smartmatchResponse => {
 				handleResponse(el, smartmatchResponse);
 			})
@@ -147,6 +149,10 @@ export default (flags) => {
 	if(flags.get('nativeAds') && flags.get('nativeAdsArticle')) {
 
 		const el = document.querySelector('.promoted-content__first');
+		
+		if(!el) {
+			return;
+		}
 
 		if(window.oAds && window.oAds.config('dfp_targeting')) {
 			initPaidPost(el, flags, window.oAds);
