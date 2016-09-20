b0880cc5afae2a92bb40e191c35af428f8e31f7d
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-09-20 14:44:29 +0100

    Remove unnecessary babel transforms post node 6 upgrade

--- server/setup.js
+++ server/setup.js
@@ -10,11 +10,7 @@ const es6Modules = [
 require('babel-register')({
 	plugins: [
 		require.resolve('babel-plugin-add-module-exports'),
-		require.resolve('babel-plugin-array-includes'),
-		require.resolve('babel-plugin-transform-es2015-destructuring'),
-		require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
-		require.resolve('babel-plugin-transform-es2015-parameters'),
-		require.resolve('babel-plugin-transform-es2015-spread')
+		require.resolve('babel-plugin-transform-es2015-modules-commonjs')
 	],
 	presets: [
 		require.resolve('babel-preset-react')
